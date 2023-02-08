b264443af2427cefb365651b174c5a96c3a58d8d
shonminh <shonminh@gmail.com>
2023-02-09 01:12:02 +0800

    implement cron_task_repo.go

---
+++ apps/collect/internal/repository/cron_task_repo.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+
+	"github.com/Shonminh/bilibee/apps/collect"
+	"github.com/Shonminh/bilibee/apps/collect/internal/repository/model"
+)
+
+type CronTaskRepoImpl struct {
+	Db *gorm.DB
+}
+
+const batchSize = 100
+
+func (impl *CronTaskRepoImpl) CreateCronTask(ctx context.Context, row model.CronTaskTab) (err error) {
+	if err = impl.Db.CreateInBatches(&row, batchSize).Error; err != nil {
+		return errors.Wrapf(err, "CreateInBatches, row=%+v", row)
+	}
+	return nil
+}
+
+func (impl *CronTaskRepoImpl) QueryUndoCronTaskList(ctx context.Context, limit int) (res []model.CronTaskTab, err error) {
+	err = impl.Db.Model(&model.CronTaskTab{}).Where("task_status = ? ", collect.TaskStatusUndo).Order("update_time DESC").Limit(limit).Find(&res).Error
+	return res, errors.Wrap(err, "QueryUndoCronTaskList")
+}
+
+func (impl *CronTaskRepoImpl) UpdateCronTaskInfo(ctx context.Context, taskId string, updateArgs map[string]interface{}) (err error) {
+	err = impl.Db.Model(&model.CronTaskTab{}).Where("task_id = ? ", taskId).Updates(updateArgs).Error
+	return errors.Wrapf(err, "UpdateCronTaskInfo, task_id=%s, updateArgs=%+v, err=%+v", taskId, updateArgs, err)
+}
--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/deckarep/golang-set/v2 v2.1.0
 	github.com/iyear/biligo v0.1.5
 	github.com/pkg/errors v0.9.1
+	gorm.io/gorm v1.24.5
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
