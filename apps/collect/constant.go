b3c49de896fee753e670d46f2a9992eb6717f31c
shonminh <shonminh@gmail.com>
2023-02-13 00:54:58 +0800

    add op status enum

--- apps/collect/constant.go
+++ apps/collect/constant.go
@@ -3,10 +3,17 @@ package collect
 type CronTaskStatus int
 
 const (
-	TaskStatusUndo CronTaskStatus = 0
+	TaskStatusUndo CronTaskStatus = iota
 	TaskStatusDone CronTaskStatus = 1
 )
 
 func (t CronTaskStatus) Uint32() uint32 {
 	return uint32(t)
 }
+
+type OpStatus int
+
+const (
+	OpStatusUndo OpStatus = iota
+	OpStatusDone
+)
